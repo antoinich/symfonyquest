5b1e2c7ab748e7141973b2c8d448dca938a640ec
antoinich <antoinejen@hotmail.com>
2019-05-13 17:07:34 +0200

    symfony4

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -9,12 +9,26 @@ use Symfony\Component\Routing\Annotation\Route;
 class BlogController extends AbstractController
 {
     /**
-     * @Route("/blog", name="blog_index")
+     * @Route("/blog/{page}", requirements={"page"="\d*"}, name="blog_index")
      */
     public function index()
     {
         return $this->render('blog/index.html.twig', [
             'owner' => 'Thomas',
+        ]);
+    }
+
+    /**
+     * @Route("/blog/show/{slug}", requirements={"slug"="[a-z0-9-]*"}, name="blog_slug")
+     */
+    public function show($slug = "article sans titre")
+    {
+        if(strlen($slug) === 0 ) {
+            $slug = "article-sans-titre";
+        }
+        $slug = ucwords(str_replace('-', ' ', $slug));
+        return $this->render('blog/show.html.twig', [
+            'slug' => $slug,
         ]);
     }
 }
